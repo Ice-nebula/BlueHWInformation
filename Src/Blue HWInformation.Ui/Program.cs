313bf78231dcd6b5aa9141d090df581de22c814c
ice-Frozen <soponhantae@gmail.com>
2022-02-13 13:21:53 +0700

    initial commit code.

---
+++ Src/Blue HWInformation.Ui/Program.cs
@@ -0,0 +1,23 @@
+﻿using Blue_HWInformation.Ui.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blue_HWInformation.Ui
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
